971ac6f7113fbf36852d8d099aa2756beb7a09a9
ImminentFate <fatefulproductions@hotmail.com>
2017-11-18 23:45:36 +1000

    Update chocolateyinstall.ps1

--- CompactGUI/tools/chocolateyinstall.ps1
+++ CompactGUI/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿#https://github.com/ImminentFate/CompactGUI
 $packageName      = 'compactgui'
 $toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$exeFile          = "CompactGUI.2.3.0.exe"
+$exeFile          = "CompactGUI.2.3.3.exe"
 $url              = "$toolsDir\$exeFile"
 
 Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\CompactGUI.lnk" -targetPath $url
