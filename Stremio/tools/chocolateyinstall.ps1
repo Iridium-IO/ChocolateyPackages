989cdedf707af1f0222cac948f7b43ecf256279b
ImminentFate <imminentfate@outlook.com>
2017-12-16 09:26:49 +1000

    Update CompactGUI

---
+++ Stremio/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'stremio'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://dl.strem.io/Stremio%203.6.5.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+
+  softwareName  = 'Stremio*'
+  checksum      = '82ECE57D75280377A38D46225364261E'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
